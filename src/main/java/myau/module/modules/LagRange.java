a659a0774b3f89cad3f0d115ea1383f53b321966
PlutoCrystal <3675907141@qq.com>
2026-05-31 16:40:26 +0800

    Add files via upload

--- src/main/java/myau/module/modules/LagRange.java
+++ src/main/java/myau/module/modules/LagRange.java
@@ -14,16 +14,15 @@ import myau.util.RenderUtil;
 import myau.util.RotationUtil;
 import myau.util.TeamUtil;
 import myau.property.properties.*;
-import myau.property.properties.BooleanProperty;
-import myau.property.properties.ModeProperty;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C02PacketUseEntity;
 import net.minecraft.network.play.client.C07PacketPlayerDigging;
-import net.minecraft.network.play.client.C07PacketPlayerDigging.Action;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Vec3;
@@ -39,13 +38,23 @@ public class LagRange extends Module {
     private boolean hasTarget = false;
     private Vec3 lastPosition = null;
     private Vec3 currentPosition = null;
-    public final IntProperty delay = new IntProperty("delay", 150, 0, 1000);
-    public final FloatProperty range = new FloatProperty("range", 10.0F, 3.0F, 100.0F);
-    public final BooleanProperty weaponsOnly = new BooleanProperty("weapons-only", true);
-    public final BooleanProperty allowTools = new BooleanProperty("allow-tools", false, this.weaponsOnly::getValue);
-    public final BooleanProperty botCheck = new BooleanProperty("bot-check", true);
-    public final BooleanProperty teams = new BooleanProperty("teams", true);
-    public final ModeProperty showPosition = new ModeProperty("show-position", 0, new String[]{"NONE", "DEFAULT", "HUD"});
+    private boolean working = true;
+    private boolean release = false;
+    private long lastReleaseTime = 0;
+
+    public final BooleanProperty dynamic = new BooleanProperty("Dynamic", true);
+    public final IntProperty minDelay = new IntProperty("MinDelay", 350, 0, 2000);
+    public final IntProperty maxDelay = new IntProperty("MaxDelay", 400, 0, 2000);
+    public final IntProperty coolDownTimer = new IntProperty("CoolDown", 0, 0, 1000);
+    public final FloatProperty attackRange = new FloatProperty("AttackRange", 3.0F, 0.0F, 6.0F);
+    public final FloatProperty initialRange = new FloatProperty("InitialRange", 8.0F, 0.0F, 12.0F);
+    public final IntProperty hurtTime = new IntProperty("HurtTime", 3, 0, 10);
+    public final BooleanProperty onlyAura = new BooleanProperty("OnlyAura", true);
+    public final BooleanProperty weaponsOnly = new BooleanProperty("WeaponsOnly", true);
+    public final BooleanProperty allowTools = new BooleanProperty("AllowTools", false, this.weaponsOnly::getValue);
+    public final BooleanProperty botCheck = new BooleanProperty("BotCheck", true);
+    public final BooleanProperty teams = new BooleanProperty("Teams", true);
+    public final ModeProperty showPosition = new ModeProperty("ShowPosition", 0, new String[]{"NONE", "DEFAULT", "HUD"});
 
     private boolean isValidTarget(EntityPlayer entityPlayer) {
         if (entityPlayer != mc.thePlayer && entityPlayer != mc.thePlayer.ridingEntity) {
@@ -67,12 +76,25 @@ public class LagRange extends Module {
         if (packet instanceof C02PacketUseEntity) {
             return true;
         } else if (packet instanceof C07PacketPlayerDigging) {
-            return ((C07PacketPlayerDigging) packet).getStatus() != Action.RELEASE_USE_ITEM;
+            return ((C07PacketPlayerDigging) packet).getStatus() != C07PacketPlayerDigging.Action.RELEASE_USE_ITEM;
         } else if (packet instanceof C08PacketPlayerBlockPlacement) {
             ItemStack item = ((C08PacketPlayerBlockPlacement) packet).getStack();
             return item == null || !(item.getItem() instanceof ItemSword);
         } else {
             return false;
+        }
+    }
+
+    private boolean isPlayerMovingCloser(EntityPlayer target) {
+        if (currentPosition == null || lastPosition == null) return true;
+        double prevDist = target.getDistanceSq(lastPosition.xCoord, lastPosition.yCoord, lastPosition.zCoord);
+        double currDist = target.getDistanceSq(currentPosition.xCoord, currentPosition.yCoord, currentPosition.zCoord);
+        return currDist < prevDist;
+    }
+
+    private void resetCoolDownTimer() {
+        if (this.tickIndex > 0) {
+            lastReleaseTime = System.currentTimeMillis();
         }
     }
 
@@ -88,6 +110,8 @@ public class LagRange extends Module {
                     Myau.lagManager.setDelay(0);
                     this.hasTarget = false;
                     BedNuker bedNuker = (BedNuker) Myau.moduleManager.modules.get(BedNuker.class);
+                    KillAura ka = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
+
                     if ((!bedNuker.isEnabled() || !bedNuker.isReady())
                             && !((IAccessorPlayerControllerMP) mc.playerController).getIsHittingBlock()
                             && (!mc.thePlayer.isUsingItem() || mc.thePlayer.isBlocking())
@@ -96,38 +120,58 @@ public class LagRange extends Module {
                                     || ItemUtil.hasRawUnbreakingEnchant()
                                     || this.allowTools.getValue() && ItemUtil.isHoldingTool()
                     )) {
-                        List<EntityPlayer> players = mc.theWorld
-                                .loadedEntityList
-                                .stream()
-                                .filter(entity -> entity instanceof EntityPlayer)
-                                .map(entity -> (EntityPlayer) entity)
-                                .filter(this::isValidTarget)
-                                .collect(Collectors.toList());
-                        if (players.isEmpty()) {
-                            this.tickIndex = -1;
-                        } else {
-                            double height = mc.thePlayer.getEyeHeight();
-                            Vec3 eyePosition = Myau.lagManager.getLastPosition().addVector(0.0, height, 0.0);
-                            Vec3 targetEyePosition = new Vec3(mc.thePlayer.lastTickPosX, mc.thePlayer.lastTickPosY + height, mc.thePlayer.lastTickPosZ);
-                            Vec3 playerEyePosition = new Vec3(mc.thePlayer.posX, mc.thePlayer.posY + height, mc.thePlayer.posZ);
-                            for (EntityPlayer player : players) {
-                                double distance = RotationUtil.distanceToBox(player, playerEyePosition);
-                                if (!(distance > (double) this.range.getValue())) {
-                                    double targetDist = RotationUtil.distanceToBox(player, targetEyePosition);
-                                    double eyeDist = RotationUtil.distanceToBox(player, eyePosition);
-                                    if (distance < targetDist || distance < eyeDist) {
-                                        if (this.tickIndex < 0) {
-                                            this.tickIndex = 0;
-                                            for (this.delayCounter = this.delayCounter + (long) this.delay.getValue();
-                                                 this.delayCounter > 0L;
-                                                 this.delayCounter = this.delayCounter - 50
-                                            ) {
-                                                this.tickIndex++;
-                                            }
+                        if (ka.isEnabled() || !this.onlyAura.getValue()) {
+                            List<EntityPlayer> players = mc.theWorld
+                                    .loadedEntityList
+                                    .stream()
+                                    .filter(entity -> entity instanceof EntityPlayer)
+                                    .map(entity -> (EntityPlayer) entity)
+                                    .filter(this::isValidTarget)
+                                    .collect(Collectors.toList());
+                            if (players.isEmpty()) {
+                                this.tickIndex = -1;
+                            } else {
+                                double height = mc.thePlayer.getEyeHeight();
+                                Vec3 eyePosition = Myau.lagManager.getLastPosition().addVector(0.0, height, 0.0);
+                                Vec3 targetEyePosition = new Vec3(mc.thePlayer.lastTickPosX, mc.thePlayer.lastTickPosY + height, mc.thePlayer.lastTickPosZ);
+                                Vec3 playerEyePosition = new Vec3(mc.thePlayer.posX, mc.thePlayer.posY + height, mc.thePlayer.posZ);
+                                for (EntityPlayer player : players) {
+                                    double distance = RotationUtil.distanceToBox(player, playerEyePosition);
+                                    if (!(distance > (double) this.initialRange.getValue())) {
+                                        if (distance >= (double) this.attackRange.getValue()) {
+                                            working = true;
                                         }
-                                        Myau.lagManager.setDelay(this.tickIndex);
-                                        this.hasTarget = true;
-                                        return;
+                                        if (!working) {
+                                            resetCoolDownTimer();
+                                            continue;
+                                        }
+
+                                        if (!isPlayerMovingCloser(player)) {
+                                            resetCoolDownTimer();
+                                            continue;
+                                        }
+
+                                        if (System.currentTimeMillis() - lastReleaseTime < coolDownTimer.getValue()) {
+                                            continue;
+                                        }
+
+                                        double targetDist = RotationUtil.distanceToBox(player, targetEyePosition);
+                                        double eyeDist = RotationUtil.distanceToBox(player, eyePosition);
+                                        if (distance < targetDist || distance < eyeDist) {
+                                            if (this.tickIndex < 0) {
+                                                this.tickIndex = 0;
+                                                long delayMs = minDelay.getValue() + (long) (Math.random() * (maxDelay.getValue() - minDelay.getValue()));
+                                                for (this.delayCounter = this.delayCounter + delayMs;
+                                                     this.delayCounter > 0L;
+                                                     this.delayCounter = this.delayCounter - 50
+                                                ) {
+                                                    this.tickIndex++;
+                                                }
+                                            }
+                                            Myau.lagManager.setDelay(this.tickIndex);
+                                            this.hasTarget = true;
+                                            return;
+                                        }
                                     }
                                 }
                             }
@@ -151,9 +195,26 @@ public class LagRange extends Module {
     @EventTarget
     public void onPacket(PacketEvent event) {
         if (this.isEnabled()) {
-            if (this.shouldResetOnPacket(event.getPacket())) {
+            Packet<?> packet = event.getPacket();
+            if (packet instanceof C02PacketUseEntity && ((C02PacketUseEntity) packet).getAction() == C02PacketUseEntity.Action.ATTACK) {
+                Entity entity = ((C02PacketUseEntity) packet).getEntityFromWorld(mc.theWorld);
+                if (entity instanceof EntityLivingBase) {
+                    EntityLivingBase target = (EntityLivingBase) entity;
+                    if (target.hurtTime <= this.hurtTime.getValue()) {
+                        release = true;
+                        working = false;
+                        Myau.lagManager.setDelay(0);
+                        this.tickIndex = -1;
+                        lastReleaseTime = System.currentTimeMillis();
+                    } else {
+                        working = true;
+                    }
+                }
+            }
+            if (this.shouldResetOnPacket(packet)) {
                 Myau.lagManager.setDelay(0);
                 this.tickIndex = -1;
+                lastReleaseTime = System.currentTimeMillis();
             }
         }
     }
@@ -207,10 +268,12 @@ public class LagRange extends Module {
         this.hasTarget = false;
         this.lastPosition = null;
         this.currentPosition = null;
+        this.working = true;
+        this.release = false;
     }
 
     @Override
     public String[] getSuffix() {
-        return new String[]{String.format("%dms", this.delay.getValue())};
+        return new String[]{String.format("%dms", this.minDelay.getValue())};
     }
 }
