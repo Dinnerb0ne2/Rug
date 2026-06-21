472fb7da647c5e8318edd4d3bd275c82b7ce771f
Dinnerb0ne2 <tomma_2022@outlook.com>
2026-06-21 19:07:19 +0800

    change code base

--- src/main/java/myau/module/modules/NoClickDelay.java
+++
@@ -1,9 +0,0 @@
-package myau.module.modules;
-
-import myau.module.Module;
-
-public class NoClickDelay extends Module {
-    public NoClickDelay() {
-        super("NoClickDelay", true, true);
-    }
-}
